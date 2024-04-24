1edeff81492e76e047ce051e2fdd66d985ef5b8b
ymoriyoshi <ymoriyoshi@iodata.jp>
2024-04-24 17:34:14 +0900

    🎉 MongoDBとNode.jsの接続

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const userRoute = require("./routes/users")
+const authRoute = require("./routes/auth")
+const postRoute = require("./routes/posts")
+const PORT = 3000;
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+// DB接続
+mongoose
+    .connect(process.env.MONGOURL)
+    .then(() => {
+    console.log("DB接続")
+})
+.catch((err) => {
+    console.log(err)
+});
+
+//ミドルウェア
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
+
+
+app.get("/", (req, res) => {
+    res.send("aaa");
+});
+
+// app.get("/users", (req, res) => {
+//     res.send("users express");
+// });
+
+app.listen(PORT, () => console.log("起動"))
