0c0f9d8b6045c248792bc52c0320e5e1a19da1cf
ymoriyoshi <ymoriyoshi@iodata.jp>
2024-04-25 17:52:44 +0900

    ✨ ユーザー情報のセキュリティUP

--- routes/users.js
+++ routes/users.js
@@ -40,7 +40,9 @@ router.delete("/:id", async(req, res) => {
 router.get("/:id", async(req, res) => {
     try{
         const user = await User.findById(req.params.id);
-        res.status(200).json(user)
+        // 分割代入
+        const{ password, updatedAt, ...other } = user._doc;
+        res.status(200).json(other);
     }catch(err){
         return res.status(500).json(err);
     } 
