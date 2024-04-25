7dc5925dd049c1d0efd9b370023f54cca5190587
ymoriyoshi <ymoriyoshi@iodata.jp>
2024-04-25 16:13:39 +0900

    ✨ ログイン機能の作成

--- routes/auth.js
+++ routes/auth.js
@@ -16,6 +16,21 @@ router.post("/register",async(req, res) => {
     }
 });
 
+//ログイン
+router.post("/login", async (req, res) => {
+    try{
+        const user = await User.findOne({email: req.body.email});
+        if (!user) return res.status(404).send("ユーザーが見つかりません");
+
+        const vailedPassword = req.body.password === user.password;
+        if(!vailedPassword) return res.status(400).json("パスワードが違います");
+
+        return res.status(200).json(user);
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+});
+
 // router.get("/", (req, res) => {
 //     res.send("auth router");
 // });
